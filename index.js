f0e7a1c3b42d7c9d8ed72e687f4d7ce01ad18654
Ahmed Nabi Khan <26043220+ahmednabik@users.noreply.github.com>
2023-06-20 03:20:12 +0500

    complete first working version

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-let container = document.querySelector(".container");
-let box = document.createElement("div");
+document.getElementById("select-squares").addEventListener("input", generateBoxes);
+document.getElementById("select-squares").addEventListener("input", displayCurrentGridSelection);
+
+function generateBoxes() {
+    let rangeSelector = document.getElementById("select-squares");
+    let numberOfSquares = rangeSelector.value;
+    let container = document.querySelector(".play-area");
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    for (i = 1; i <= (numberOfSquares * numberOfSquares); i++) {
+        let box = document.createElement("div");
+        box.classList.add("box");
+        box.style.width = (768 / numberOfSquares) + "px";
+        box.style.height = (768 / numberOfSquares) + "px";
+        box.addEventListener("mouseenter", paintBoxes)
+        container.appendChild(box);
+    }
 
 
-container.addEventListener("on")
+}
+function paintBoxes(event) {
+    event.target.style.backgroundColor = "rgb(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) +")";
+    console.log("mouse entered");
+}
+function displayCurrentGridSelection() {
+    let rangeSelector = document.getElementById("select-squares");
+    let numberOfSquares = rangeSelector.value;
+    let displayDiv = document.getElementById("selected-squares");
+    displayDiv.textContent = numberOfSquares + " x " + numberOfSquares;
+
+}
