a06989abcc2a46a05028486cda20d2a1aed3cf49
Ahmed Nabi Khan <26043220+ahmednabik@users.noreply.github.com>
2023-06-21 01:40:11 +0500

    add new grid selection. add clear buttion. updated logic

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-document.getElementById("select-squares").addEventListener("input", generateBoxes);
-document.getElementById("select-squares").addEventListener("input", displayCurrentGridSelection);
+generateBoxes();
+let resolutionSelector = document.querySelectorAll('input[name = "radio-btn"]');
+console.log (resolutionSelector);
+resolutionSelector.forEach(rs => { rs.addEventListener("change", generateBoxes); 
+});
+
+document.getElementById("clear-btn").addEventListener("click", function () {
+    let container = document.querySelector(".play-area");
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    generateBoxes();
+})
 
 function generateBoxes() {
-    let rangeSelector = document.getElementById("select-squares");
+    let rangeSelector = document.querySelector('input[name = "radio-btn"]:checked');
     let numberOfSquares = rangeSelector.value;
     let container = document.querySelector(".play-area");
     while (container.firstChild) {
@@ -21,11 +32,4 @@ function generateBoxes() {
 }
 function paintBoxes(event) {
     event.target.style.backgroundColor = "rgb(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) +")";
-}
-function displayCurrentGridSelection() {
-    let rangeSelector = document.getElementById("select-squares");
-    let numberOfSquares = rangeSelector.value;
-    let displayDiv = document.getElementById("selected-squares");
-    displayDiv.textContent = numberOfSquares + " x " + numberOfSquares;
-
 }
